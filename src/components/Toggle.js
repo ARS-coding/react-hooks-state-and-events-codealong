076daacb22cfc2fd140a46a85ea3d669e4923e34
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-14 19:51:01 +0300

    Add my initial solution in Toggle component.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+  // function clickEventHandler(event) {
+  //   event.target.firstChild.data === "OFF" ? event.target.firstChild.data = "ON" : event.target.firstChild.data = "OFF";
+  // }
+
+  return <button onClick={clickEventHandler}>OFF</button>;
 }
 
 export default Toggle;
