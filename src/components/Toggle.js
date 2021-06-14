cc5f3e2ca59aff8e23e0ff9249e8f825082ec912
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-14 20:50:45 +0300

    Takes some notes.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,12 +1,31 @@
 import React from "react";
+import {useState} from "react";
+
 
 function Toggle() {
+
+  // My answer starts
 
   // function clickEventHandler(event) {
   //   event.target.firstChild.data === "OFF" ? event.target.firstChild.data = "ON" : event.target.firstChild.data = "OFF";
   // }
 
-  return <button onClick={clickEventHandler}>OFF</button>;
+  // My answer ends
+
+  const [isOn, setIsOn] = useState(false);
+
+  function clickEventHandler(e) {
+    setIsOn((isOn) => !isOn)
+  }
+
+  return (
+    <button 
+      style={{backgroundColor : isOn ? "blue" : "red"}}
+      onClick={clickEventHandler}
+    >
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
